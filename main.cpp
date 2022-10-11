6c2221469dff5fa7a9bd4e71e54b55a0fa828304
Mustafa Alwash <tofenofal99@gmail.com>
2022-10-11 11:20:54 +0300

    adding the particle object and the physics of movement

--- main.cpp
+++ main.cpp
@@ -20,7 +20,56 @@ public:
     void render(sf::RenderWindow& rend){
         rend.draw(sun);
     }
+    
+    void get_force(){
+        return force;
+    }
+    
 }
+
+
+
+class particle{
+    sf::Vector2f pos;
+    sf::Vector2f vel;
+    sf::CircleShape sun;
+    
+public:
+    particle(float pos_x ,float pos_y ,float vel_x ,float vel_y){
+        pos.x = pos_x;
+        pos.y = pos_y;
+        vel.x = vel_x;
+        vel.y = vel_y;
+    }
+    
+    sun.setPosition(pos);
+    sun.setFullColor(sf::Color::Yellow);
+    sun.setRadius(5);
+    
+    void render(sf::RenderWindow& rend){
+        rend.draw(sun);
+    }
+    
+    void physics(source &S){
+        float dist_x = s.get_pos().x - pos.x;
+        float dist_y = s.get_pos().y - pos.y;
+        float distance = sqrt(dist_x*dist_x + dist_y*dist_y);
+        
+        float inverse_dist = 1.f/distance;
+        
+        float normalized_x = inverse_dist * dist_x;
+        float normalized_y = inverse_dist * dist_y;
+        
+        float inverse_square_law = inverse_dist * inverse_dist;
+        
+        float accel_x = normalized_x * s.get_force() * inverse_square_law;
+        float accel_y = normalized_y * s.get_force() * inverse_square_law;
+        
+        
+        
+    }
+}
+
 
 
 int main(){
